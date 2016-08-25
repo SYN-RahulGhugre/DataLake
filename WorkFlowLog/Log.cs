cbdeb6a921401cd6057c958a9f4be3f50ab42a2d
Rahul Ghugre <rahul.ghugre@synechron.com>
2016-08-25 12:13:12 -0400

    Test commit

--- WorkFlowLog/Log.cs
+++ WorkFlowLog/Log.cs
@@ -17,7 +17,7 @@ namespace WorkFlowLog
         /// <param name="exceptionJSON">ExceptionJSON Parameter</param>
         public void InsertWorkFlowLog(int workflowDefinitionID, int logLevel, string message, string exceptionType, string exceptionJSON)
         {
-            using (var db = new DataLakeEntities())
+            using (var db = new DataLakeEntities()) 
             {
                 var logWorkFlowId = db.uspGetLogParameter(workflowDefinitionID).FirstOrDefault();
                 int insertWorkflowLog = db.uspInsertWorkflowLog(logWorkFlowId.WorkFlowRunID, logWorkFlowId.WorkflowTaskID, logLevel, message, DateTime.UtcNow, exceptionType, exceptionJSON);
